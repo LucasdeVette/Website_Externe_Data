16f5d786e7256ae447750a8f2c91847c4c018f20
Lucas <lucas.devette@edu-kw1c.nl>
2026-04-24 12:23:05 +0200

    beschrijving van je wijzigingen

--- javascript_files/index.js
+++ javascript_files/index.js
@@ -1,5 +1,9 @@
 console.log("Mijn naam is Lucas");
 console.log("Ik leer javascript");
 let naam = "Lucas";
-let leeftijd = "19";
-console.log('mijn naam is ${naam} en ik ben ${leeftijd} jaar oud');
+let leeftijd = 19;
+console.log(`mijn naam is ${naam} en ik ben ${leeftijd} jaar oud`);
+console.log (`test ${1 + 1}`);
+if (leeftijd >= 18) {
+    console.log ("je bent volwassen");
+}
