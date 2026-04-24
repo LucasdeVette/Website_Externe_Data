bc05db0b2825aca4bfea80f8f8b2ed3a921a3498
Lucas <lucas.devette@edu-kw1c.nl>
2026-04-24 11:03:01 +0200

    Nieuwe Commit

---
+++ javascript_files/index.js
@@ -0,0 +1,5 @@
+console.log("Mijn naam is Lucas");
+console.log("Ik leer javascript");
+let naam = "Lucas";
+let leeftijd = "19";
+console.log('mijn naam is ${naam} en ik ben ${leeftijd} jaar oud');
